ab862efa5dcee2212df8bfc0cb8e3cfd7b8ecac6
Atman Shah <atman1339@gmail.com>
2023-03-26 23:02:27 +0530

    28th Video

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -21,7 +21,49 @@ function LoginForm({ existingUser }) {
     FirebaseAuthService.logoutUser();
   }
 
-  return <h1>Login Form</h1>;
+  return (
+    <div className="login-form-container">
+      {existingUser ? (
+        <div className="row">
+          <h3>Welcome, {existingUser.email}</h3>
+          <button
+            type="button"
+            className="primary-button"
+            onClick={handleLogout}
+          >
+            Logout
+          </button>
+        </div>
+      ) : (
+        <form onSubmit={handleSubmit} className="login-form">
+          <label className="input-label login-label">
+            Username (email):
+            <input
+              type="email"
+              required
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              className="input-text"
+            />
+          </label>
+
+          <label className="input-label login-label">
+            Password:
+            <input
+              type="password"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="input-text"
+            />
+          </label>
+          <div className="button-box">
+            <button className="primary-button">Submit</button>
+          </div>
+        </form>
+      )}
+    </div>
+  );
 }
 
 export default LoginForm;
