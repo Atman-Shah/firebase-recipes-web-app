23667b64acb1fec2556fb9690b4b9de9a22c98cf
Atman Shah <atman1339@gmail.com>
2023-03-26 22:19:25 +0530

    27th Video

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,6 +2,25 @@ import { useState } from "react";
 import FirebaseAuthService from "../FirebaseAuthService";
 
 function LoginForm({ existingUser }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    try {
+      await FirebaseAuthService.registerUser(username, password);
+      setUsername("");
+      setPassword("");
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
+  function handleLogout() {
+    FirebaseAuthService.logoutUser();
+  }
+
   return <h1>Login Form</h1>;
 }
 
