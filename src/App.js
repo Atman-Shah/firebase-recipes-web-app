4ca7ab18a86ceef48dbe450097b0fd578a8a730d
Atman Shah <atman1339@gmail.com>
2023-03-28 22:06:00 +0530

    49th Video

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import FirebaseAuthService from "./FirebaseAuthService";
 import LoginForm from "./components/LoginForm";
 import AddEditRecipeForm from "./components/AddEditRecipeForm";
@@ -8,8 +8,54 @@ import FirebaseFirestoreService from "./FirebaseFirestoreService";
 
 function App() {
   const [user, setUser] = useState(null);
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    fetchRecipes()
+      .then((fetchedRecipes) => {
+        setRecipes(fetchedRecipes);
+      })
+      .catch((error) => {
+        console.error(error.message);
+        throw error;
+      });
+  }, [user]);
 
   FirebaseAuthService.subscribeToAuthChanges(setUser);
+
+  async function fetchRecipes() {
+    let fetchedRecipes = [];
+
+    try {
+      const response = await FirebaseFirestoreService.readDocuments("recipes");
+
+      const newRecipes = response.docs.map((recipeDoc) => {
+        const id = recipeDoc.id;
+        const data = recipeDoc.data();
+        data.publishDate = new Date(data.publishDate.seconds * 1000);
+
+        return { ...data, id };
+      });
+
+      fetchedRecipes = [...newRecipes];
+    } catch (error) {
+      console.eroor(error.message);
+      throw error;
+    }
+
+    return fetchedRecipes;
+  }
+
+  async function handleFetchRecipes() {
+    try {
+      const fetchedRecipes = await fetchRecipes();
+
+      setRecipes(fetchedRecipes);
+    } catch (error) {
+      console.error(error.message);
+      throw error;
+    }
+  }
 
   async function handleAddRecipe(newRecipe) {
     try {
@@ -19,6 +65,7 @@ function App() {
       );
 
       //TODO: fetch new recipes from firestore
+      handleFetchRecipes();
 
       alert(`successfully created a recipe with an ID = ${response.id}`);
     } catch (error) {
