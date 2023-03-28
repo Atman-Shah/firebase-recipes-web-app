80b30fd468ad67a90c3466707b00a1d66224d45d
Atman Shah <atman1339@gmail.com>
2023-03-28 15:17:18 +0530

    40th Video

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import { useState } from "react";
 import FirebaseAuthService from "./FirebaseAuthService";
 import LoginForm from "./components/LoginForm";
+import AddEditRecipeForm from "./components/AddEditRecipeForm";
+
 import "./App.css";
+import FirebaseFirestoreService from "./FirebaseFirestoreService";
 
 function App() {
   const [user, setUser] = useState(null);
 
   FirebaseAuthService.subscribeToAuthChanges(setUser);
 
+  async function handleAddRecipe(newRecipe) {
+    try {
+      const response = await FirebaseFirestoreService.createDocument(
+        "recipes",
+        newRecipe
+      );
+
+      //TODO: fetch new recipes from firestore
+
+      alert(`successfully created a recipe with an ID = ${response.id}`);
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
   return (
     <div className="App">
       <div className="title-row">
         <h1 className="title">Firebase Recipes</h1>
         <LoginForm existingUser={user}></LoginForm>
+      </div>
+      <div className="main">
+        <AddEditRecipeForm
+          handleAddRecipe={handleAddRecipe}
+        ></AddEditRecipeForm>
       </div>
     </div>
   );
---
+++ src/components/AddEditRecipeForm.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+function AddEditRecipeForm({ handleAddRecipe }) {
+  const [name, setName] = useState("");
+  const [category, setCategory] = useState("");
+  const [publishDate, setPublishDate] = useState(
+    new Date().toISOString().split("T")[0]
+  );
+  const [directions, setDirections] = useState("");
+  const [ingredients, setIngredients] = useState([]);
+  const [ingredientName, setIngredientName] = useState("");
+
+  return <h1>Add edit recipe form</h1>;
+}
+
+export default AddEditRecipeForm;
