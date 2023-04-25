28cb641af181b00c17608e9e8ae4bd900210a3be
Atman Shah <atman1339@gmail.com>
2023-04-25 22:33:08 +0530

    74th Video

--- src/components/ImageUploadPreview.js
+++ src/components/ImageUploadPreview.js
@@ -1,4 +1,28 @@
-function ImageUploadPreview() {
+import { useState, useRef, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+import FirebaseStorageService from "../FirebaseStorageService";
+
+function ImageUploadPreview({
+  basePath,
+  existingImageUrl,
+  handleUploadFinish,
+  handliUploadCancel,
+}) {
+  const [uploadProgress, setUploadProgress] = useState(-1);
+  const [imageUrl, setImageUrl] = useState("");
+
+  const fileInputRef = useRef();
+
+  useEffect(() => {
+    if (existingImageUrl) {
+      setImageUrl(existingImageUrl);
+    } else {
+      setImageUrl("");
+      fileInputRef.current.value = null;
+    }
+  }, [existingImageUrl]);
+
   return (
     <div className="image-upload-preview-container">
       hello from image upload preview
