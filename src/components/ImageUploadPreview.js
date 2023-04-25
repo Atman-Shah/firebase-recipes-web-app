3257752b6c1c97a56839523a7779af21e72b54ef
Atman Shah <atman1339@gmail.com>
2023-04-25 22:55:35 +0530

    75th Video

--- src/components/ImageUploadPreview.js
+++ src/components/ImageUploadPreview.js
@@ -7,7 +7,7 @@ function ImageUploadPreview({
   basePath,
   existingImageUrl,
   handleUploadFinish,
-  handliUploadCancel,
+  handleUploadCancel,
 }) {
   const [uploadProgress, setUploadProgress] = useState(-1);
   const [imageUrl, setImageUrl] = useState("");
@@ -22,6 +22,42 @@ function ImageUploadPreview({
       fileInputRef.current.value = null;
     }
   }, [existingImageUrl]);
+
+  async function handleFileChanged(event) {
+    const files = event.target.files;
+    const file = files[0];
+
+    if (!file) {
+      alert("File select failed, Please try agian.");
+      return;
+    }
+
+    const generatedFileId = uuidv4();
+
+    try {
+      const downloadUrl = await FirebaseStorageService.uploadFile(
+        file,
+        `${basePath}/${generatedFileId}`,
+        setUploadProgress
+      );
+
+      setImageUrl(downloadUrl);
+      handleUploadFinish(downloadUrl);
+    } catch (error) {
+      setUploadProgress(-1);
+      fileInputRef.current.value = null;
+      alert(error.message);
+      throw error;
+    }
+  }
+
+  function handleCancelImageClick() {
+    FirebaseStorageService.deleteFile(imageUrl);
+    fileInputRef.current.value = null;
+    setImageUrl("");
+    setUploadProgress(-1);
+    handleUploadCancel();
+  }
 
   return (
     <div className="image-upload-preview-container">
