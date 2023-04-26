bd2e84d857cf1a1a5ae21ded871206a2e3157dfc
Atman Shah <atman1339@gmail.com>
2023-04-26 17:01:28 +0530

    83rd Video

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,5 @@
-const functions = require("firebase-functions");
-
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
-});
+const FirebaseConfig = require("./FirebaseConfig");
+const functions = FirebaseConfig.functions;
+const firestore = FirebaseConfig.firestore;
+const storageBucket = FirebaseConfig.storageBucket;
+const admin = FirebaseConfig.admin;
