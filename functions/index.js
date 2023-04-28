1e238dc66bf0e100d22e25a20fd1309de0ee8f1d
Atman Shah <atman1339@gmail.com>
2023-04-28 22:22:49 +0530

    85th Video

--- functions/index.js
+++ functions/index.js
@@ -7,7 +7,7 @@ const admin = FirebaseConfig.admin;
 exports.onCreateRecipe = functions.firestore
   .document("recipes/{recipeId}")
   .onCreate(async (snapshot) => {
-    functions.logger.log(JSON.stringify(functions.config()));
+    // functions.logger.log(JSON.stringify(functions.config()));
     const countDocRef = firestore.collection("recipeCounts").doc("all");
     const countDoc = await countDocRef.get();
 
@@ -20,17 +20,65 @@ exports.onCreateRecipe = functions.firestore
     const recipe = snapshot.data();
 
     if (recipe.isPublished) {
-      const counPublishedRef = firestore
+      const countPublishedRef = firestore
         .collection("recipeCounts")
-        .doc("piblished");
-      const counPublished = await counPublishedRef.get();
+        .doc("published");
+      const counPublished = await countPublishedRef.get();
 
       if (counPublished.exists) {
-        counPublishedRef.update({
+        countPublishedRef.update({
           count: admin.firestore.FieldValue.increment(1),
         });
       } else {
-        counPublishedRef.set({ count: 1 });
+        countPublishedRef.set({ count: 1 });
+      }
+    }
+  });
+
+exports.onDeleteRecipe = functions.firestore
+  .document("recipes/{recipeId}")
+  .onDelete(async (snapshot) => {
+    const recipe = snapshot.data();
+    const imageUrl = recipe.imageUrl;
+
+    if (imageUrl) {
+      const decodedUrl = decodeURIComponent(imageUrl);
+      const startIndex = decodedUrl.indexOf("/o/") + 3;
+      const endIndex = decodedUrl.indexOf("?");
+      const fullFilePath = decodedUrl.substring(startIndex, endIndex);
+      const file = storageBucket.file(fullFilePath);
+
+      console.log(`Attempting to delete: ${fullFilePath}`);
+
+      try {
+        await file.delete();
+        console.log("we have successfully deleted image.");
+      } catch (error) {
+        console.log(`Failed to delete file : ${error.message}`);
+      }
+    }
+
+    const countDocRef = firestore.collection("recipeCounts").doc("all");
+    const countDoc = await countDocRef.get();
+
+    if (countDoc.exists) {
+      countDocRef.update({ count: admin.firestore.FieldValue.increment(-1) });
+    } else {
+      countDocRef.set({ count: 0 });
+    }
+
+    if (recipe.isPublished) {
+      const countPublishedRef = firestore
+        .collection("recipeCounts")
+        .doc("published");
+      const countPublished = await countPublishedRef.get();
+
+      if (countPublished.exists) {
+        countPublishedRef.update({
+          count: admin.firestore.FieldValue.increment(-1),
+        });
+      } else {
+        countPublishedRef.set({ count: 0 });
       }
     }
   });
